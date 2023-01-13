1d1d62a2a5e6f5ae9ec539306b103e9327c72e3b
k0r1 <mimnhquan.k0r1@gmail.com>
2023-01-13 13:31:43 +0700

    Whitelist

--- contracts/token/KoriSecurityToken.sol
+++ contracts/token/KoriSecurityToken.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /**
  * @title STO Token
@@ -10,13 +14,15 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `ERC20` functions.
  */
-contract KoriSTO is ERC20 {
+contract KoriSTO is ERC20, ReentrancyGuard, Ownable, AccessControl {
     using SafeMath for uint256;
+    using SafeERC20 for IERC20;
 
     // Variable
     uint256 public fundraisingGoal;
     uint256 public raised;
     uint256 public tokenPrice;
+    address[] public whitelistedAddresses;
 
     // Mapping
     mapping(address => uint256) raiser;
@@ -39,6 +45,7 @@ contract KoriSTO is ERC20 {
 
     // Invest user get token
     function invest() public payable {
+        require(isWhitelisted(msg.sender));
         require(msg.value > 0);
         require(msg.value >= tokenPrice);
         require(raised.add(msg.value) <= fundraisingGoal);
@@ -50,5 +57,35 @@ contract KoriSTO is ERC20 {
         if (raised == fundraisingGoal) {
             emit FundRaised();
         }
+    }
+
+    function addToWhitelist(address _address) public onlyOwner {
+        require(
+            _address != address(0),
+            "Cannot add address 0x0 to the whitelist."
+        );
+        whitelistedAddresses.push(_address);
+    }
+
+    function removeFromWhitelist(address _address) public onlyOwner {
+        uint256 index = 0;
+        for (uint256 i = 0; i < whitelistedAddresses.length; i++) {
+            if (whitelistedAddresses[i] == _address) {
+                index = i;
+                break;
+            }
+        }
+        whitelistedAddresses[index] = whitelistedAddresses[
+            whitelistedAddresses.length - 1
+        ];
+    }
+
+    function isWhitelisted(address _address) public view returns (bool) {
+        for (uint256 i = 0; i < whitelistedAddresses.length; i++) {
+            if (whitelistedAddresses[i] == _address) {
+                return true;
+            }
+        }
+        return false;
     }
 }
